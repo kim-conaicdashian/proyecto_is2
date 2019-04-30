49bb0399a4a04d25afec99d9ab5d6b8d3e384273
Jose Figueroa <pervasivetie23@gmail.com>
2019-04-30 04:09:51 -0700

    Agregar vistas login y registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,162 @@
 @extends('layouts.app')
 @section('content')
+    <style>
+        :root {
+        --input-padding-x: 1.5rem;
+        --input-padding-y: .75rem;
+        }
+
+        body {
+        background: #007bff;
+        background: linear-gradient(to right, #00E633, #33AEFF);
+        }
+
+        .card-signin {
+        border: 0;
+        border-radius: 1rem;
+        box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+        overflow: hidden;
+        }
+
+        .card-signin .card-title {
+        margin-bottom: 2rem;
+        font-weight: 300;
+        font-size: 1.5rem;
+        }
+
+        .card-signin .card-img-left {
+        width: 45%;
+        /* Link to your background image using in the property below! */
+        background: scroll center url('https://source.unsplash.com/WEQbe2jBg40/414x512');
+        background-size: cover;
+        }
+
+        .card-signin .card-body {
+        padding: 2rem;
+        }
+
+        .form-signin {
+        width: 100%;
+        }
+
+        .form-signin .btn {
+        font-size: 80%;
+        border-radius: 5rem;
+        letter-spacing: .1rem;
+        font-weight: bold;
+        padding: 1rem;
+        transition: all 0.2s;
+        }
+
+        .form-label-group {
+        position: relative;
+        margin-bottom: 1rem;
+        }
+
+        .form-label-group input {
+        height: auto;
+        border-radius: 2rem;
+        }
+
+        .form-label-group>input,
+        .form-label-group>label {
+        padding: var(--input-padding-y) var(--input-padding-x);
+        }
+
+        .form-label-group>label {
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: block;
+        width: 100%;
+        margin-bottom: 0;
+        /* Override default `<label>` margin */
+        line-height: 1.5;
+        color: #495057;
+        border: 1px solid transparent;
+        border-radius: .25rem;
+        transition: all .1s ease-in-out;
+        }
+
+        .form-label-group input::-webkit-input-placeholder {
+        color: transparent;
+        }
+
+        .form-label-group input:-ms-input-placeholder {
+        color: transparent;
+        }
+
+        .form-label-group input::-ms-input-placeholder {
+        color: transparent;
+        }
+
+        .form-label-group input::-moz-placeholder {
+        color: transparent;
+        }
+
+        .form-label-group input::placeholder {
+        color: transparent;
+        }
+
+        .form-label-group input:not(:placeholder-shown) {
+        padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
+        padding-bottom: calc(var(--input-padding-y) / 3);
+        }
+
+        .form-label-group input:not(:placeholder-shown)~label {
+        padding-top: calc(var(--input-padding-y) / 3);
+        padding-bottom: calc(var(--input-padding-y) / 3);
+        font-size: 12px;
+        color: #777;
+        }
+
+        .btn-google {
+        color: white;
+        background-color: #ea4335;
+        }
+
+        .btn-facebook {
+        color: white;
+        background-color: #3b5998;
+        }
+    </style>
+
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="card">
-                    <div class="card-header"><h3 class="text-center">Acceder</h3></div>
+        <div class="row">
+            <div class="col-lg-10 col-xl-9 mx-auto">
+                <div class="card card-signin flex-row my-5">
+                    <div class="card-img-left d-none d-md-flex">
+                        <!-- Background image for card set in CSS! -->
+                    </div>
                     <div class="card-body">
-                            @if (Session::has('message'))
-                                <div class="alert alert-warning">{{ Session::get('message') }}</div>
-                            @endif
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    @foreach ($errors->all() as $error)
-                                        {{$error}}
-                                    @endforeach
-                                </div> 
-                            @endif
+                        @if (Session::has('message'))
+                            <div class="alert alert-warning">{{ Session::get('message') }}</div>
+                        @endif
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                    {{$error}}
+                                @endforeach
+                            </div> 
+                        @endif
+                        <h5 class="card-title text-center">Acceder</h5>
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
-                            <div class="form-group row">
-                                <label for="email" class="col-sm-4 col-form-label text-md-right">Correo Electronico</label>
-                                <div class="col-md-6">
-                                    <input type="email" class="form-control " name="email" placeholder="Ingresar email" value="{{ old('email') }}" required>
-                                </div>
+                
+                            <div class="form-label-group">
+                                <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Ingresar email" value="{{ old('email') }}" required>
+                                <label for="inputEmail">Correo Electronico</label>
                             </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña</label>
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control " name="password" placeholder="Ingresar contraseña" required>
-                                </div>
+                            
+                            <hr>
+                
+                            <div class="form-label-group">
+                                <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Ingresar contraseña" required>
+                                <label for="inputPassword">Contraseña</label>
                             </div>
-
-                            <div class="form-group row mb-4">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
-                                    <a class="btn btn-link" href="register">{{ __('No tienes una cuenta? Resgistrate.') }}</a>
-                                </div>
-                            </div>
+                
+                            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">{{ __('Login') }}</button>
+                            <a class="d-block text-center mt-2 small" href="register">{{ __('No tienes una cuenta? Resgistrate.') }}</a>
                         </form>
                     </div>
                 </div>
