c292687ffb0ad57feb1abd4048fd8303caa0b15c
Jose Figueroa <pervasivetie23@gmail.com>
2019-05-12 18:37:18 -0700

    Agrega vista aseptable a editar perfil.

--- resources/views/academicos/editarPerfil.blade.php
+++ resources/views/academicos/editarPerfil.blade.php
@@ -1,45 +1,51 @@
 @extends('layouts.app')
 @section('content')
 <title>Editar Perfil</title>
-    <form method= "POST" action="{{ route('academico.updatePerfil', $academico->id) }}">
-        @csrf
-        @method('PUT')
-        <div class="form-group row">
-            <label for="nombre" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
-            <div class="col-md-6">
-                <input id="nombre" type="text" class="form-control" name="nombre" value="{{$academico->nombre}}">
+    <div  class="container background-style shadow border-0 my-5">
+        <h2 class="text-center">Edita tu Perfil</h2>
+        <br>
+        <form method= "POST" action="{{ route('academico.updatePerfil', $academico->id) }}">
+            @csrf
+            @method('PUT')
+            <div class="form-group row">
+                <label for="nombre" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
+                <div class="col-md-6">
+                    <input id="nombre" type="text" class="form-control" name="nombre" value="{{$academico->nombre}}">
+                </div>
             </div>
-        </div>
 
-        <div class="form-group row">
-            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electronico') }}</label>
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{$academico->email}}">
-                {!! $errors->first('email', '<span style="color:red;">:message</span>') !!}
+            <div class="form-group row">
+                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electronico') }}</label>
+                <div class="col-md-6">
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{$academico->email}}">
+                    {!! $errors->first('email', '<span style="color:red;">:message</span>') !!}
+                </div>
             </div>
-        </div>
 
-        <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="knhdl +w-" placeholder="Contraseña">
+            <div class="form-group row">
+                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
+                <div class="col-md-6">
+                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="knhdl +w-" placeholder="Contraseña">
+                </div>
             </div>
-        </div>
 
-        <div class="form-group row">
-            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Contraseña') }}</label>
-            <div class="col-md-6">
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" value="knhdl +w-" placeholder="Contraseña">
+            <div class="form-group row">
+                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Contraseña') }}</label>
+                <div class="col-md-6">
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" value="knhdl +w-" placeholder="Contraseña">
+                </div>
             </div>
-        </div>
 
-        <div class="form-group row mb-4">
-            <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-info" style="float:right">
-                        <span class="fa fa-edit"></span>
-                    {{ __('Editar') }}
-                </button>
+            <div class="form-group row mb-4">
+                <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-info" style="float:right">
+                            <span class="fa fa-edit"></span>
+                        {{ __('Editar') }}
+                    </button>
+                </div>
             </div>
-        </div>
-    </form>
+        </form>
+        <br>
+        <br>
+    </div>
 @endsection
