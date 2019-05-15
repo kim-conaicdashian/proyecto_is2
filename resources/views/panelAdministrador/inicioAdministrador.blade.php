2f333f66427f017c8c634a89971d68020a79d7e5
Fabian <fabian6_03@hotmail.com>
2019-05-14 22:22:01 -0700

    Agregar variable planes agrupados a la vista inicioAdministrador
    Cree la variable planesAgrupados en el controlador vistasController en la funcion panelAdmin(), para asi acomodar los planes que tengan una categoria repetida

--- resources/views/panelAdministrador/inicioAdministrador.blade.php
+++ resources/views/panelAdministrador/inicioAdministrador.blade.php
@@ -28,47 +28,58 @@
                             <th>Generar reporte del plan</th>
                         </tr>
                     </thead>
-                    
+                    {{-- Asigno el valor de todos los planes agrupados a la variable collection --}}
+                    @php
+                        $collection = $planesAgrupados;
+                    @endphp
+                    {{--  --}}
                     <tbody>
-
-                    @foreach ($planes as $plan)
+                        
+                        @foreach ($collection as $loquequiera =>$planesAgrupados)
                         <tr>
+                            {{-- @php
+                                dd($key);
+                            @endphp --}}
                             <td>
-                                {{$plan->recomendacion->categoria->nombre}}
+                                {{$planesAgrupados[0]->recomendacion->categoria->nombre}}
                             </td>
-
-                            <td>
-                                {{$plan->recomendacion->nombre}}
-                            </td>
-                            
-                            <td>
-                                <a href="{{route('plan.show',$plan->id)}}">{{$plan->nombre}}</a>
-                            </td>
-                            <td>
-                                {{$plan->fecha_termino}}
-                            </td>
-                            
-                            @if ($plan->completado== 1)
-                                <td>
-                                    <p style="text-align:center">Sí</p>
-                                </td>
-                            @else
-                                <td>
-                                    <p style="text-align:center">No</p>
-                                </td>
-                            @endif
-                            
-                            <td>
-                                <div class="col-lg-3 center-block" style="position: relative;text-align:center;left: 30%;">
-                                    <a style="color:white !important;" class="btn btn-success btn-sm" href="{{ route('categoria.reporte', $plan->recomendacion->categoria->id) }}">
-                                        <span class="fa fa-download"></span> 
-                                        
-                                    </a>
-                                </div>
-                            </td>
-                            
-                        </tr>
-                    @endforeach
+                            @foreach ($planesAgrupados as $plan)
+                                @if (!$loop->first)
+                                    <td></td>
+                                @endif
+                                    <td>
+                                        {{$plan->recomendacion->nombre}}
+                                    </td>
+                                    
+                                    <td>
+                                        <a href="{{route('plan.show',$plan->id)}}">{{$plan->nombre}}</a>
+                                    </td>
+                                    <td>
+                                        {{$plan->fecha_termino}}
+                                    </td>
+                                    
+                                    @if ($plan->completado== 1)
+                                        <td>
+                                            <p style="text-align:center">Sí</p>
+                                        </td>
+                                    @else
+                                        <td>
+                                            <p style="text-align:center">No</p>
+                                        </td>
+                                    @endif
+                                    
+                                    <td>
+                                        <div class="col-lg-3 center-block" style="position: relative;text-align:center;left: 30%;">
+                                            <a style="color:white !important;" class="btn btn-success btn-sm" href="{{ route('categoria.reporte', $plan->recomendacion->categoria->id) }}">
+                                                <span class="fa fa-download"></span> 
+                                            </a>
+                                        </div>
+                                    </td>
+                                    
+                                </tr>
+                            @endforeach
+                        @endforeach
+                        
                     </tbody>
                 </table>
             @else
